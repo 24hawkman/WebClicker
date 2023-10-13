7ed51ee3442b5616a0cec362a3a6ff57a48a21a0
24hawkman <hawks4mszr@gmail.com>
2023-10-13 12:58:14 -0400

    updated webdriver capabilities and cleaned up clicker application

--- jj_click.py
+++ jj_click.py
@@ -1,25 +1,64 @@
 import sys
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+'''
+    WebDriver - using selenium python library
+    ------------------------------------------
+    * Used for accessing web pages via chrome emulation web browser
+      and making interactions with html elements/forms
+
+    * chromedriver.exe must reside in the same directory
+    
+    * Must use webdriver packages containing single chromedriver.exe file
+      -> https://googlechromelabs.github.io/chrome-for-testing/#stable
+'''
 
 votes = 0
-driver = webdriver.Chrome()
-driver.get('https://biletnikoffaward.com/fan-vote')
-
+totalVotes = 0
+service = Service()
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=options)
 
 while True:
-    #driver = webdriver.Chrome()
-    if driver.find_element_by_id('PDI_answer48276570'):
-        button = driver.find_element_by_id('PDI_answer48276570')
+    driver.get('https://www.knoxnews.com/story/sports/high-school/2023/10/09/vote-for-the-prepxtra-girls-athlete-of-the-week-for-oct-1-7/71026432007/')
+    driver.implicitly_wait(2)
+
+    if driver.find_elements(By.CLASS_NAME, 'gnt_mol_xb'): # locating cover add X
+        driver.find_element(By.CLASS_NAME, 'gnt_mol_xb').click() #close cover add
+    
+    # locate any nested iframes and switch driver to that frame
+    frame = driver.find_element(By.CLASS_NAME, 'gnt_em_pd_if')
+    driver.switch_to.frame(frame)
+
+    if driver.find_element(By.ID, "PDI_answer57785574"):
+        # make selection
+        button = driver.find_element(By.ID, "PDI_answer57785574")
         button.click()
-        submit = driver.find_element_by_id('pd-vote-button10415962')
+
+        # submit form
+        submit = driver.find_element(By.ID, "pd-vote-button12850052")
         submit.click()
+
         time.sleep(2)
-        driver.refresh()
-        #driver.close()
         votes += 1
-        print (str(votes))
+        totalVotes += 1
+        print ('submission count: ' + str(totalVotes))
+
+        # reset driver after specific duration
+        #  - this helps prevent server blacklisting due to recognized automation
+        if votes >= 20:
+            driver.quit()
+            time.sleep(60)
+            service = Service()
+            options = webdriver.ChromeOptions()
+            driver = webdriver.Chrome(service=service, options=options)
+            votes = 0
+
+        #driver.close()   
     else:
         driver.refresh()
 
-# initial percentile: 34.45%
