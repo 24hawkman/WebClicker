dcdc64b4f5f56a8fa8456f365a6ca4b33508d040
24hawkman <hawks4mszr@gmail.com>
2025-11-01 14:40:50 -0400

    Code updates, removed chromedriver.exe and instead installing at application runtime

--- jj_click.py
+++ jj_click.py
@@ -19,28 +19,28 @@ from selenium.webdriver.common.by import By
 
 votes = 0
 totalVotes = 0
-service = Service()
+service = Service(ChromeDriverManager().install())
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=options)
 
 while True:
-    driver.get('https://www.knoxnews.com/story/sports/high-school/2023/10/09/vote-for-the-prepxtra-girls-athlete-of-the-week-for-oct-1-7/71026432007/')
+    driver.get('https://biletnikoffaward.com/fan-vote/')
     driver.implicitly_wait(2)
 
     if driver.find_elements(By.CLASS_NAME, 'gnt_mol_xb'): # locating cover add X
         driver.find_element(By.CLASS_NAME, 'gnt_mol_xb').click() #close cover add
     
     # locate any nested iframes and switch driver to that frame
-    frame = driver.find_element(By.CLASS_NAME, 'gnt_em_pd_if')
-    driver.switch_to.frame(frame)
+    # frame = driver.find_element(By.CLASS_NAME, 'gnt_em_pd_if')
+    # driver.switch_to.frame(frame)
 
-    if driver.find_element(By.ID, "PDI_answer57785574"):
+    if driver.find_element(By.ID, "PDI_answer70813967"):
         # make selection
-        button = driver.find_element(By.ID, "PDI_answer57785574")
+        button = driver.find_element(By.ID, "PDI_answer70813967")
         button.click()
 
         # submit form
-        submit = driver.find_element(By.ID, "pd-vote-button12850052")
+        submit = driver.find_element(By.ID, "pd-vote-button16081645")
         submit.click()
 
         time.sleep(2)
@@ -52,7 +52,7 @@ while True:
         #  - this helps prevent server blacklisting due to recognized automation
         if votes >= 20:
             driver.quit()
-            time.sleep(60)
+            time.sleep(15)
             service = Service()
             options = webdriver.ChromeOptions()
             driver = webdriver.Chrome(service=service, options=options)
